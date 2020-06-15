64ff1ea6245aab28d8af3bae34061a8a403d4f97
guricerin <gurizumi@gmail.com>
2020-06-15 17:59:41 +0900

    update

--- setup.ps1
+++ setup.ps1
@@ -46,13 +46,16 @@ function MakeSymlinkPath($path) {
 function main() {
     $dotfiles_home = "$SCRIPT_ROOT\home"
 
-    # dotfiles
-    Get-ChildItem -LiteralPath $dotfiles_home -File -Filter ".*" -Force |
+    Get-ChildItem -LiteralPath $dotfiles_home -File -Force -Recurse | 
     ForEach-Object {
         $sourceFile = $_.FullName
-        $targetFile = "$env:UserProfile\$($_.name)"
-        if (Test-Path "$targetFile") {
-            if ((ReadLink -path $targetFile) -ne $sourceFile) {
+        $targetFile = MakeSymlinkPath -path $sourceFile
+        $parent = Split-Path $targetFile -Parent
+        if (!(Test-Path -Path $parent)) {
+            mkdir -Path "$parent" -Force > $null
+        }
+        if (Test-Path -Path $targetFile) {
+            if ((ReadLink -path "$targetFile") -ne $sourceFile) {
                 $answer = AskConfirmation -message "'$targetFile' already exists, do you want to overwrite it?"
                 if (AnswerIsYes -answer $answer) {
                     Rename-Item -LiteralPath "$targetFile" -NewName "${targetFile}.bak" -Force > $null
@@ -68,54 +71,8 @@ function main() {
             }
         }
         else {
-            New-Item -ItemType SymbolicLink -Path $targetFile -Value $sourceFile > $null
+            New-Item -ItemType SymbolicLink -Path "$targetFile" -Value "$sourceFile" > $null
             PrintResult -success $? -message "$targetFile → $sourceFile"
-        }
-    }
-
-    # home
-    Get-ChildItem -LiteralPath $dotfiles_home -Directory -Force | 
-    ForEach-Object {
-        $dir_root = $_.FullName
-
-        # create folder tree
-        $targetFolder = MakeSymlinkPath -path $dir_root
-        if (!(Test-Path -LiteralPath "$targetFolder")) {
-            mkdir -Path "$targetFolder" -Force > $null
-        }
-        Get-ChildItem -LiteralPath $dir_root -Directory -Recurse |
-        ForEach-Object {
-            $targetFolder = MakeSymlinkPath -path $dir_root
-            if (!(Test-Path -LiteralPath "$targetFolder")) {
-                mkdir -Path "$targetFolder" -Force > $null
-            }
-        }
-
-        # synlink files
-        Get-ChildItem -LiteralPath $dir_root -File -Force -Recurse |
-        ForEach-Object {
-            $sourceFile = $_.FullName
-            $targetFile = MakeSymlinkPath -path $sourceFile
-            if (Test-Path -Path $targetFile) {
-                if ((ReadLink -path "$targetFile") -ne $sourceFile) {
-                    $answer = AskConfirmation -message "'$targetFile' already exists, do you want to overwrite it?"
-                    if (AnswerIsYes -answer $answer) {
-                        Rename-Item -LiteralPath "$targetFile" -NewName "${targetFile}.bak" -Force > $null
-                        New-Item -ItemType SymbolicLink -Path "$targetFile" -Value "$sourceFile" > $null
-                        PrintResult -success $? -message "$targetFile → $sourceFile"
-                    }
-                    else {
-                        PrintError -message "$targetFile → $sourceFile"
-                    }
-                }
-                else {
-                    PrintSuccess -message "$targetFile → $sourceFile"
-                }
-            }
-            else {
-                New-Item -ItemType SymbolicLink -Path "$targetFile" -Value "$sourceFile" > $null
-                PrintResult -success $? -message "$targetFile → $sourceFile"
-            }
         }
     }
 }
