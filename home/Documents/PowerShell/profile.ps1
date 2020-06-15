ec18dfaec6bf51ebf20e3176a6bc55ef2b4a7a76
guricerin <gurizumi@gmail.com>
2020-06-15 15:29:22 +0900

    update

--- home/Documents/PowerShell/profile.ps1
+++ home/Documents/PowerShell/profile.ps1
@@ -1,7 +1,6 @@
 ﻿# -------------------------------------------------------------------------------------------
-# My-Util todo:baseパス指定
-. "C:\Users\guriz\Documents\PowerShell\Scripts\My-Util.ps1"
 
+Set-Alias -Name "ll" -Value Get-ChildItem
 Set-Alias -Name "note" -Value "C:\bin\Notepad++\notepad++.exe"
 
 # -------------------------------------------------------------------------------------------
@@ -21,22 +20,5 @@ function Prompt() {
 $GitPromptSettings.IndexForegroundColor = "Green"
 $GitPromptSettings.WorkingForegroundColor = 'Red'
 $GitPromptSettings.LocalWorkingStatusForegroundColor = 'Red'
-
-# -------------------------------------------------------------------------------------------
-# Get-ChildItemColor(色付きls)
-Import-Module Get-ChildItemColor
-
-# lsコマンドの動作をよりunix風に
-Set-Alias ll Get-ChildItem -Option AllScope
-Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
-
-# ファイル毎の表示色を明るめに変更
-$GetChildItemColorTable.File['Directory'] = "Cyan"
-foreach ($Exe in $GetChildItemColorExtensions.ExecutableList) {
-    $GetChildItemColorTable.File[$Exe] = "Green"
-}
-foreach ($Src in $GetChildItemColorExtensions.SourceCodeList) {
-    $GetChildItemColorTable.File[$Src] = "Green"
-}
 
 # -------------------------------------------------------------------------------------------
