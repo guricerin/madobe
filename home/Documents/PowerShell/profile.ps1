0b2b1cae7549d22c2f49abc21895171954fe9e30
guricerin <gurizumi@gmail.com>
2020-09-10 13:03:29 +0900

    git status alias

--- home/Documents/PowerShell/profile.ps1
+++ home/Documents/PowerShell/profile.ps1
@@ -4,6 +4,10 @@ Set-Alias -Name "l" -Value Get-ChildItem
 Set-Alias -Name "ll" -Value Get-ChildItem
 Set-Alias -Name "note" -Value "C:\bin\Notepad++\notepad++.exe"
 
+function gs() {
+    git status
+}
+
 # -------------------------------------------------------------------------------------------
 # posh-git
 Import-Module posh-git
