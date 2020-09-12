2e9a433743fd1f092df83c2a2cabd90da4d11465
guricerin <gurizumi@gmail.com>
2020-09-12 15:42:32 +0900

    update

--- home/Documents/PowerShell/profile.ps1
+++ home/Documents/PowerShell/profile.ps1
@@ -1,4 +1,5 @@
 ﻿# -------------------------------------------------------------------------------------------
+# alias
 
 Set-Alias -Name "l" -Value Get-ChildItem
 Set-Alias -Name "ll" -Value Get-ChildItem
@@ -12,17 +13,7 @@ function gs() {
 # posh-git
 Import-Module posh-git
 
-# プロンプトの上書き
-function Prompt() {
-    Write-Host("[{0}@{1}] {2}" -f $env:UserName, $env:ComputerName, $pwd.ProviderPath) -nonewline
-    Write-VcsStatus
-    Write-Host("") # 改行
-    return "> "
-}
-
-# []の中の色彩度を上げる
-$GitPromptSettings.IndexForegroundColor = "Green"
-$GitPromptSettings.WorkingForegroundColor = 'Red'
-$GitPromptSettings.LocalWorkingStatusForegroundColor = 'Red'
+$GitPromptSettings.DefaultPromptPrefix = "[${env:UserName}@${env:ComputerName}] "
+$GitPromptSettings.DefaultPromptSuffix = "`n> "
 
 # -------------------------------------------------------------------------------------------
