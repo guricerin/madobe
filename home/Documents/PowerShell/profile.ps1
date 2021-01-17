9e7066dd483376081a861d8357c80b90e4a331a8
guricerin <chanbo1e9@gmail.com>
2021-01-17 09:34:36 +0900

    docker

--- home/Documents/PowerShell/profile.ps1
+++ home/Documents/PowerShell/profile.ps1
@@ -1,16 +1,20 @@
 ﻿# -------------------------------------------------------------------------------------------
-# alias
+## alias
 
+# ls
 Set-Alias -Name "l" -Value Get-ChildItem
 Set-Alias -Name "ll" -Value Get-ChildItem
 Set-Alias -Name "note" -Value "C:\bin\Notepad++\notepad++.exe"
+# docker
+Set-Alias -Name "d" -Value docker
+Set-Alias -Name "dc" -Value docker-compose
 
 function gs() {
     git status
 }
 
 # -------------------------------------------------------------------------------------------
-# posh-git
+## posh-git
 Import-Module posh-git
 
 $GitPromptSettings.DefaultPromptPrefix = "[${env:UserName}@${env:ComputerName}] "
